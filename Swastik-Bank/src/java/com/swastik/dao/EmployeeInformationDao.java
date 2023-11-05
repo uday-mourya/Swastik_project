ad1520e645fdc69e3c32be67a143da9390c217b5
Sohan Maali <sohanmaali4.com>
2023-11-05 12:03:17 +0530

    dao classes add by sohan

---
+++ Swastik-Bank/src/java/com/swastik/dao/EmployeeInformationDao.java
@@ -0,0 +1,55 @@
+package com.swastik.dao;
+
+public class EmployeeInformationDao {
+
+    private int employeeId;
+    private String name, email, doh, mobile, password;
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDoh() {
+        return doh;
+    }
+
+    public void setDoh(String doh) {
+        this.doh = doh;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
